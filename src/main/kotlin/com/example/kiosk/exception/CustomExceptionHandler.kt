cedee8ae2d90c39007f5e7907b1a6c8421125426
Tech5818 <pqow5818@gmail.com>
2024-12-04 16:22:12 +0900

    Feat: Validation Exception Custom

--- src/main/kotlin/com/example/kiosk/exception/CustomExceptionHandler.kt
+++ src/main/kotlin/com/example/kiosk/exception/CustomExceptionHandler.kt
@@ -1,13 +1,22 @@
 package com.example.kiosk.exception
 
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class CustomExceptionHandler {
-    @ExceptionHandler
-    fun handleException(exception: CustomException): ResponseEntity<ErrorResponseBody> {
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun handleValidateException(exception: HttpMessageNotReadableException): ResponseEntity<ErrorResponseBody> {
+        return ErrorResponseEntity(CustomException(ErrorCode.BAD_REQUEST, "유효성 검사 실패")).toResponseEntity()
+    }
+
+    @ExceptionHandler(CustomException::class)
+    fun handleException(exception: CustomException, request: HttpServletRequest): ResponseEntity<ErrorResponseBody> {
         return ErrorResponseEntity(exception).toResponseEntity()
     }
+
 }
